1268b1af1ef0be7f2f9077ca6bd2d9d45691e524
Chris <chrisstudio1016@gmail.com>
2025-06-07 21:59:48 +0800

    knowledge graph page default selection

--- web/src/app/(main)/graph/components/knowledge-base-list.tsx
+++ web/src/app/(main)/graph/components/knowledge-base-list.tsx
@@ -3,17 +3,22 @@
 import { knowledgeBaseData } from "@/samples/knowledge-base";
 import KnowledgeBaseItem from "./knowledge-base-item";
 import { KnowledgeBaseModel } from "@/models/knowledge-base";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import useKnowledgeBaseSearchStore from "@/store/use-knowledge-base-search-store";
 
 const KnowledgeBaseList = () => {
   const [selectedKnowledgeBase, setSelectedKnowledgeBase] =
     useState<KnowledgeBaseModel>();
   const { searchInput } = useKnowledgeBaseSearchStore();
+  const knowledgeBases = knowledgeBaseData;
+
+  useEffect(() => {
+    setSelectedKnowledgeBase(knowledgeBases[0]);
+  }, []);
 
   return (
     <div className="flex flex-col h-[90%] overflow-y-auto gap-4">
-      {knowledgeBaseData.map((knowledgeBase, index: number) => {
+      {knowledgeBases.map((knowledgeBase, index: number) => {
         if (
           knowledgeBase.name.toLowerCase().includes(searchInput.toLowerCase())
         ) {
